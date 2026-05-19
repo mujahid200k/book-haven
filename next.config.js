0e06bf928b9ff6eee2d5d8badc659f1cded500e5
mujahid200k <mujahid20lacdollar@gmail.com>
2026-05-19 14:37:11 +0600

    fix: disable cache for auth routes

--- next.config.js
+++ next.config.js
@@ -8,7 +8,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 }
- 
+
 module.exports = nextConfig
- 
